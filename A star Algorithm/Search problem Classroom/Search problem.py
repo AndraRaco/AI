831e435626ddd065b08807a03e09c833f86d9b54
Racovita Andra <andra.rac@gmai.com>
2020-04-20 15:12:52 +0300

    Update Search problem

--- A star Algorithm/Search problem Classroom/Search problem.py
+++ A star Algorithm/Search problem Classroom/Search problem.py
@@ -1,21 +1,38 @@
 """ Problema de cautare (mesaj intre elevii unei clase) """
 
+import time
+import math
+
+# Declarare
+numar_euristica = None  # Numarul euristicii pe care o folosim
+n = None  # Numar coloane
+m = None  # Numar linii
+matrice_clasa = None  # Matricea cu elevi
+mesaj_trimis_de = None  # Numele celui care trimite mesajul
+mesaj_primit_de = None  # Numele celui care primeste mesajul
+pozitii_in_banci = None  # Un dictionar cu pozitiile elevilor in banci
+poz_destinatar = None  # Pozitia celui care trimite biletul
+poz_primire = None  # Pozitia celui care trebuie sa primeasca biletul
+configuratie_initiala = None  # Configuratia initiala, pozitia biletului la inceput
+suparati = None  # Lista cu elevi suparati
+fisier_output = None  # Fisierul in care scriem rezultatele
+
 
 def pozitionare(matrice):
     poz = {}  # Dictionar in care retinem pozitiile fiecarui elev
-    # retinem perechi (i,j)
+    # retinem perechi (st/dr, i, j) st=0, dr=1
     for i in range(m):  # Matricea e n*m
         for j in range(n):
             poz[matrice[i][j][0]] = (0, i, j)  # Sta in stanga in banca
             poz[matrice[i][j][1]] = (1, i, j)  # Sta in dreapta in banca
-    if "liber" in poz:
+    if "liber" in poz:  # Stergem "liber"
         del poz["liber"]
     return poz
 
 
-class Configuratie:  # Constructor de copiere pentru matrice
+class Configuratie:
     def __init__(self, pozitie_bilet):
-        self.pozitie_bilet = pozitie_bilet
+        self.pozitie_bilet = pozitie_bilet  # (0/1, i, j) 0-stanga, 1-dreapta
 
     def __repr__(self):
         return f"{self.pozitie_bilet}"
@@ -23,7 +40,21 @@ class Configuratie:  # Constructor de copiere pentru matrice
     def __eq__(self, other):
         return self.pozitie_bilet == self.pozitie_bilet
 
-    def euristica(self):
+    def euristica(self, numar_euristica):
+        if numar_euristica == 1:
+            return self.euristica1()
+        elif numar_euristica == 2:
+            return self.euristica2()
+        elif numar_euristica == 3:
+            return self.euristica3()
+
+    def euristica1(self):
+        """Distanta Manhattan este cea mai mica distant care poate fi parcursa intre 2 puncte.
+        Prin urmare, distanta Manhattan va fi mereu mai mica sau egala decat adevarata distanta ce trebuie parcursa
+        de bilet pentru a putea ajunge la pozitia finala (0 ≤ h(n) ≤ h*(n)). In plus, daca nu ar exista
+        elevi suparati sau locuri libere, distanta Manhattan ar fi egala cu adevarata distanta.
+        In concluzie, euristica este consistenta, iar orice euristica consistenta este si admisibila."""
+
         # O sa verificam daca pozitia biletului si pozitia copilului la care
         # trebuie sa ajunga biletul sunt pe aceeasi coloana, daca nu sunt trebuie sa trecem
         # pe alta coloana si asta se face doar pe ultimul si penultimul rand
@@ -36,38 +67,103 @@ class Configuratie:  # Constructor de copiere pentru matrice
         poz_bilet = self.pozitie_bilet
 
         # Distanta Manhattan: abs(x1- x2) + abs(y1 – y2)
-        if (poz_bilet[2] == poz_primire[2]):  # sunt pe acelasi rand
+        if (poz_bilet[2] == poz_primire[2]):  # sunt pe aceeasi coloana
 
             if(poz_bilet[0] == poz_primire[0]):  # daca sunt in stanga in banca
                 return abs(poz_bilet[1] - poz_primire[1]) + abs(poz_bilet[2] - poz_primire[2])
             else:  # unul sta in stang si unul sta in dreapta
                 return abs(poz_bilet[1] - poz_primire[1]) + abs(poz_bilet[2] - poz_primire[2]) + 1
 
-        else:  # sunt pe randuri diferite
+        else:  # sunt pe coloane diferite
             # Transferul se face doar pe la ultima si penultima banca
 
             # Din locul in care e biletul trebuie sa mearga la penultima banca
             dif_pozB_penultima_banca = abs(m - 2 - poz_bilet[1])
             dif_pozD_penultima_banca = abs(m - 2 - poz_primire[1])
 
-            # diferenta de un rand intre ele
+            # diferenta de o coloane intre ele, adica 1 sau 2 coloane in clasa
             if abs(poz_primire[2] - poz_bilet[2]) == 1:
                 if poz_primire[0] == poz_bilet[0]:
                     return dif_pozD_penultima_banca + dif_pozB_penultima_banca + 1
                 else:
                     return dif_pozD_penultima_banca + dif_pozB_penultima_banca + 2
 
-            if abs(poz_primire[2] - poz_bilet[2]) == 2:  # diferenta de 2 randuri
+            # diferenta de 2 coloane, adica 3-4 coloane in clasa
+            if abs(poz_primire[2] - poz_bilet[2]) == 2:
                 if poz_primire[0] == poz_bilet[0]:
                     return dif_pozD_penultima_banca + dif_pozB_penultima_banca + 3
                 else:
                     return dif_pozD_penultima_banca + dif_pozB_penultima_banca + 4
 
+    def euristica2(self):
+        """Distanta Euclidiana este cea mai mica distant care poate fi parcursa intre 2 puncte.
+        Prin urmare, distanta Euclidiana va fi mereu mai mica sau egala decat adevarata distanta ce trebuie parcursa
+        de bilet pentru a putea ajunge la pozitia finala (0 ≤ h(n) ≤ h*(n)). In plus, daca nu ar exista
+        elevi suparati sau locuri libere, distanta Euclidiana ar fi egala cu adevarata distanta.
+        In concluzie, euristica este consistenta, iar orice euristica consistenta este si admisibila."""
+        # O sa verificam daca pozitia biletului si pozitia copilului la care
+        # trebuie sa ajunga biletul sunt pe aceeasi coloana, daca nu sunt trebuie sa trecem
+        # pe alta coloana si asta se face doar pe ultimul si penultimul rand
+
+        # ignoram locurile libere si supararile
+
+        global poz_primire  # Pozitia elevului care trebuie sa primeasca biletul
+        global n  # nr coloane, care este mereu 3
+        global m  # nr randuri
+        poz_bilet = self.pozitie_bilet
+
+        # Distanta Euclidiana: sqrt(abs(x1- x2)^2 + abs(y1 – y2)^2)
+        if (poz_bilet[2] == poz_primire[2]):  # sunt pe aceeasi coloana
+
+            if(poz_bilet[0] == poz_primire[0]):  # daca sunt in stanga in banca
+                return math.sqrt((abs(poz_bilet[1] - poz_primire[1])) ** 2 + (abs(poz_bilet[2] - poz_primire[2]) ** 2))
+            else:  # unul sta in stang si unul sta in dreapta
+                return math.sqrt(
+                    (abs(poz_bilet[1] - poz_primire[1])) ** 2 + (abs(poz_bilet[2] - poz_primire[2] + 1) ** 2))
+
+        else:  # sunt pe coloane diferite
+            # Transferul se face doar pe la ultima si penultima banca
+
+            # Din locul in care e biletul trebuie sa mearga la penultima banca
+            dif_pozB_penultima_banca = abs(m - 2 - poz_bilet[1])
+            dif_pozD_penultima_banca = abs(m - 2 - poz_primire[1])
+
+            # diferenta de o coloana intre ele, adica 1-2 coloane in clasa
+            if abs(poz_primire[2] - poz_bilet[2]) == 1:
+                if poz_primire[0] == poz_bilet[0]:
+                    return math.sqrt((dif_pozD_penultima_banca + dif_pozB_penultima_banca) ** 2 + 1 ** 2)
+                else:
+                    return math.sqrt((dif_pozD_penultima_banca + dif_pozB_penultima_banca) ** 2 + 2 ** 2)
+
+            # diferenta de 2 coloane, adica 3-4 coloane in realitate
+            if abs(poz_primire[2] - poz_bilet[2]) == 2:
+                if poz_primire[0] == poz_bilet[0]:
+                    return math.sqrt((dif_pozD_penultima_banca + dif_pozB_penultima_banca) ** 2 + 3 ** 2)
+                else:
+                    return math.sqrt((dif_pozD_penultima_banca + dif_pozB_penultima_banca) ** 2 + 4 ** 2)
+
+    def euristica3(self):
+        """Euristica 3 nu este o euristica admisibila, deoarece suma a mai multor euristici admisibile nu este o euristica admisibila
+        si am obligat algoritmul sa treaca pe coloana urmatoare doar pe la ultima banca.
+        Euristica este incosistenta, deoarce cand pozitia biletului este pe ultimul sau penultimul rand 
+        returnam o valoare care nu respecta monotonia."""
+        global poz_primire  # Pozitia elevului care trebuie sa primeasca biletul
+        global n  # nr coloane, care este mereu 3
+        global m  # nr randuri
+        poz_bilet = self.pozitie_bilet
+        if abs(poz_primire[2] - poz_bilet[2]) != 0:
+            # Il facem sa prefere sa treaca pe alta coloana pe ultimul rand
+            if poz_bilet[1] == m - 2:
+                return (self.euristica1() + self.euristica2()) ** 10
+            if poz_bilet[1] == m - 1:
+                return 0
+        return self.euristica1() + self.euristica2()
+
 
 class Nod:
     def __init__(self, config):
         self.info = config
-        self.h = config.euristica()
+        self.h = config.euristica(numar_euristica)
 
     def __str__(self):
         return "({}, h={})".format(self.info, self.h)
@@ -166,7 +262,7 @@ class NodParcurgere:
         """
 
         # Daca avem destinatarul pe coloana 1 si primitorul pe 3, trebuie pe coloana 2 doar sa trecem pe ultima sau penultima banca
-        # fara sa ne ridicam in sus
+        # fara sa ne ridicam in sus, doar daca e necesar(locuri libere sau suparati)
 
         global matrice_clasa  # Matricea cu numele
         global pozitii_in_banci  # Dictionar
@@ -232,7 +328,7 @@ class NodParcurgere:
                 succ.append((Nod(conf_noua), 1))
         return succ
 
-    def test_scop(self):
+    def test_scop(self):  # testez daca nodul extras din lista open este nod scop
         return self.nod_graf.info.pozitie_bilet == self.problema.nod_scop
 
     def __str__(self):
@@ -312,10 +408,13 @@ def in_lista(l, nod):
     return None
 
 
-def a_star():
+def a_star(f):
     """
         Functia care implementeaza algoritmul A-star
     """
+
+    # preiau timpul in milisecunde de dinainte de A*
+    t_inainte = int(round(time.time() * 1000))
 
     nod_curent = None
 
@@ -324,7 +423,7 @@ def a_star():
     closed = []  # closed va contine elemente de tip NodParcurgere
 
     while len(open) > 0:
-        print(str_info_noduri(open))  # afisam lista open
+        # f.write(str_info_noduri(open)+"\n")  # afisam lista open
         nod_curent = open.pop(0)  # scoatem primul element din lista open
         closed.append(nod_curent)  # si il adaugam la finalul listei closed
 
@@ -400,44 +499,145 @@ def a_star():
 
                     open.insert(i, nod_nou)
 
-    print("\n------------------ Concluzie -----------------------")
+    # Concluzie
+    # scrie_output(nod_curent, open)
     if len(open) == 0:
-        print("Lista open e vida, nu avem drum de la nodul start la nodul scop")
+        if mesaj_primit_de == mesaj_trimis_de:
+            f.write(
+                f"Lista open e vida, mesajul este deja la {mesaj_primit_de}\n")
+        else:
+            f.write(
+                f"Lista open e vida, nu avem drum de la {mesaj_trimis_de} la {mesaj_primit_de}\n")
     else:
-        print("Drum de cost minim: " + str_info_noduri(nod_curent.drum_arbore()))
+        f.write("Drum de cost minim: " +
+                str_info_noduri(nod_curent.drum_arbore())+"\n")
+
+    # preiau timpul in milisecunde de dupa A*
+    t_dupa = int(round(time.time() * 1000))
+    f.write("Calculatorul a \"gandit\" timp de " +
+            str(t_dupa - t_inainte) + " milisecunde.\n\n")
 
 
-# Input
-n = 3  # Sunt 3 coloane de banci, cate 7 elevi pe rand
-matrice_clasa = [
-    [("ionel", "alina"), ("teo", "eliza"), ("carmen", "monica")],
-    [("george", "diana"), ("bob", "liber"), ("nadia", "mihai")],
-    [("liber", "costin"), ("anda", "bogdan"), ("dora", "marin")],
-    [("luiza", "simona"), ("dana", "cristian"), ("tamara", "dragos")],
-    [("mihnea", "razvan"), ("radu", "patricia"), ("gigel", "elena")],
-    [("liber", "andrei"), ("oana", "victor"), ("liber", "dorel")],
-    [("viorel", "alex"), ("ela", "nicoleta"), ("maria", "gabi")]
-]
+def citire_input():
+    global n
+    global m
+    global matrice_clasa
+    global mesaj_trimis_de
+    global mesaj_primit_de
+    global pozitii_in_banci
+    global poz_destinatar
+    global poz_primire
+    global configuratie_initiala
+    global suparati
+    global nume_fisier_input
 
-m = len(matrice_clasa)  # 7, numarul de randuri
-# ionel ii trimite mesaj lui dragos
+    # Deschidem fisierul de input
+    f = open(nume_fisier_input, "r")
 
-mesaj_trimis_de = "ionel"
-mesaj_primit_de = "dragos"
-pozitii_in_banci = pozitionare(matrice_clasa)  # Dictionar
+    # Pe primul rand avem numele celui care trimite mesajul si numele celui care trebuie sa primeasca
+    linie = f.readline().split()
+    mesaj_trimis_de = linie[0]
+    mesaj_primit_de = linie[1]
 
-poz_destinatar = pozitii_in_banci[mesaj_trimis_de]
-poz_primire = pozitii_in_banci[mesaj_primit_de]
-configuratie_initiala = Configuratie(poz_destinatar)
+    # Numar linii si numar coloane
+    linie = f.readline().split()
+    m = (int)(linie[0])  # randuri
+    # Pun cate doi in banci, adica fac o matrice 7x3 si elemenetele sunt tupluri (colegii de banca)
+    n = ((int)(linie[1]))//2  # coloane
 
-suparati = [("george", "ionel"), ("ela", "nicoleta"), ("victor", "oana"),
-            ("teo", "eliza"), ("teo", "luiza"), ("elena", "dragos"), ("alina", "dragos")]
+    # Matricea cu asezarea in banci
+    matrice_clasa = [[0 for x in range(n)] for y in range(m)]
+    for i in range(m):
+        linie = f.readline().split()
+        k = 0
+        for j in range(n):
+            matrice_clasa[i][j] = (linie[k], linie[k + 1])
+            k += 2
+
+    # Numarul de perechi de suparati
+    linie = f.readline().split()
+    nr_perechi_suparati = (int)(linie[0])
+
+    # Perechile de suparati
+    suparati = [0 for i in range(nr_perechi_suparati)]
+    for i in range(nr_perechi_suparati):
+        linie = f.readline().split()
+        suparati[i] = (linie[0], linie[1])
 
 
 if __name__ == "__main__":
+    # Meniu pentru accesarea meniului
+    text = "Alegeti numarul inputului dorit:\n"
+    text += "1. Un fisier de input care nu are solutii\n"
+    text += "2. Un fisier de input care da o stare initiala care este si finala\n"
+    text += "3. Un fisier de input cu un drum de cost minim de lungime 3-5\n"
+    text += "4. Un fisier de input cu un drum de cost minim de lungime mai mare decat 5\n"
+    print(text)
+
+    # Repetam primim un raspuns valid
+    raspuns_valid = False
+    while not raspuns_valid:
+        global nume_fisier_input
+        tip_algoritm = input(
+            "Varianta input: ")
+        if tip_algoritm in ['1', '2', '3', '4']:
+            raspuns_valid = True
+            if tip_algoritm == '1':
+                nume_fisier_input = "input1.txt"
+                fisier_output = "output1.txt"
+            elif tip_algoritm == '2':
+                nume_fisier_input = "input2.txt"
+                fisier_output = "output2.txt"
+            elif tip_algoritm == '3':
+                nume_fisier_input = "input3.txt"
+                fisier_output = "output3.txt"
+            elif tip_algoritm == '4':
+                nume_fisier_input = "input4.txt"
+                fisier_output = "output4.txt"
+        else:
+            print("Nu ati ales o varianta corecta.")
+
+    # Apelam Algoritmul A* pentru toate euristicile
+
+    # Citire input
+    citire_input()
+
+    # Dictionar cu pozitiile in banci
+    pozitii_in_banci = pozitionare(matrice_clasa)
+
+    # Numele celui care trimite mesajul si numele celui care trebuie sa primeasca
+    poz_destinatar = pozitii_in_banci[mesaj_trimis_de]
+    poz_primire = pozitii_in_banci[mesaj_primit_de]
+
+    # Deschide fisierul de output
+    f = open(fisier_output, "w")
+
+    # Pentru fiecare euristica apelam Algoritmul A*
+    f.write("\n------------------ Concluzie -----------------------\n")
+
+    # Algoritmul A* pentru euristica 1
+    numar_euristica = 1
+    # Configuratia initiala
+    configuratie_initiala = Configuratie(poz_destinatar)
     problema = Problema()
     NodParcurgere.problema = problema
-    a_star()
+    a_star(f)
 
-# Nu-i bine ca nu-mi face drumul minim
-# cand ajunge pe randul 2 se urca in sus nu trece spre dreapta
+    # Algoritmul A* pentru euristica 2
+    numar_euristica = 2
+    # Configuratia initiala
+    configuratie_initiala = Configuratie(poz_destinatar)
+    problema = Problema()
+    NodParcurgere.problema = problema
+    a_star(f)
+
+    # Algoritmul A* pentru euristica 3
+    numar_euristica = 3
+    # Configuratia initiala
+    configuratie_initiala = Configuratie(poz_destinatar)
+    problema = Problema()
+    NodParcurgere.problema = problema
+    a_star(f)
+
+    # Inchidem output-ul
+    f.close()
